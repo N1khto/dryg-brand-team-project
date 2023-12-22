faaec4edfef9f7bde1ab1ca941d3a5b32c7e92ad
Volodymyr Bohomol <8773927@gmail.com>
2023-12-22 15:07:18 +0200

    - created Color, Size and Item models

--- backend/products/models.py
+++ backend/products/models.py
@@ -9,9 +9,51 @@ class Category(models.Model):
         return self.name
 
 
+class Color(models.Model):
+    name = models.CharField(max_length=255)
+    hex = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    class SizeValueChoices(models.TextChoices):
+        XS = "xs"
+        S = "s"
+        M = "m"
+        L = "l"
+        XL = "xl"
+        XXL = "xxl"
+        XXXL = "xxxl"
+        OVERSIZE = "oversize"
+
+    tag = models.CharField(max_length=255, help_text="please mark related product")
+    value = models.CharField(max_length=15, choices=SizeValueChoices.choices)
+    length = models.PositiveSmallIntegerField(null=True, blank=True)
+    width = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.value} for {self.tag}"
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     fabric = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True, default="")
     added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    model = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, null=True, on_delete=models.SET_NULL)
+    size = models.ForeignKey(Size, null=True, on_delete=models.SET_NULL)
+    stock = models.PositiveSmallIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+
+    def __str__(self):
+        return f"{self.model} {self.color}"
