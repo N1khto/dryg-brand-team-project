afff4d5d8377797ef3326170bf80a5f9e6a9baa0
Volodymyr Bohomol <8773927@gmail.com>
2023-12-22 13:58:50 +0200

    - created Category and Product models

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default="Category description...")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    fabric = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True, default="")
+    added = models.DateTimeField(auto_now_add=True)
