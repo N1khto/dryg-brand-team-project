1445fcbd0e1291e0169988393930e398d4e76fc4
Volodymyr Bohomol <8773927@gmail.com>
2023-12-24 14:44:14 +0200

    - added Image model - added media settings

--- backend/products/models.py
+++ backend/products/models.py
@@ -42,18 +42,29 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     fabric = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True, default="")
-    added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class Item(models.Model):
-    model = models.ForeignKey(Product, on_delete=models.CASCADE)
-    color = models.ForeignKey(Color, null=True, on_delete=models.SET_NULL)
-    size = models.ForeignKey(Size, null=True, on_delete=models.SET_NULL)
+    model = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="items")
+    color = models.ForeignKey(Color, null=True, on_delete=models.SET_NULL, related_name="items")
+    size = models.ForeignKey(Size, null=True, on_delete=models.SET_NULL, related_name="items")
     stock = models.PositiveSmallIntegerField(default=0)
     price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    stripe_product_id = models.CharField(max_length=255, blank=True, default="")
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.model} {self.color}"
+
+
+class Image(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="images/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"image for {self.item.name}"
