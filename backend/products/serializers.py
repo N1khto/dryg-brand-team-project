cad33ddd798b174ce74415e526e8df20fe125771
Volodymyr Bohomol <8773927@gmail.com>
2023-12-25 14:46:31 +0200

    - added serializers for Item and Product models

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -21,10 +21,70 @@ class SizeSerializer(serializers.ModelSerializer):
         fields = ("id", "tag", "value", "length", "width")
 
 
+class SizeListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        url = value.image.url
+        return f"{url}"
+
+
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ("id", "item", "image", "uploaded_at")
+        fields = ("image",)
+
+
+class ImageListingField(serializers.RelatedField):
+    def to_representation(self, instance):
+        url = instance.image.url
+        return f"{url}"
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ("id", "model", "color", "size", "stock", "price", "stripe_product_id", "date_added")
+
+
+class ItemSizeAvailableSerializer(ItemSerializer):
+    class Meta:
+        model = Item
+
+    def to_representation(self, instance):
+        size_value = instance.size.value
+        return f"{size_value}"
+
+
+class ItemColorAvailableSerializer(ItemSerializer):
+    class Meta:
+        model = Item
+
+    def to_representation(self, instance):
+        color_name = instance.color.name
+        return f"{color_name}"
+
+
+class ItemListSerializer(ItemSerializer):
+    model = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    color = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    size = SizeSerializer()
+    images = ImageListingField(many=True, read_only=True)
+
+    class Meta:
+        model = Item
+        fields = ("id", "model", "color", "size", "stock", "price", "stripe_product_id", "date_added", "images")
+
+
+class ItemDetailSerializer(ItemSerializer):
+    model = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    color = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    size = SizeSerializer()
+    images = ImageListingField(many=True, read_only=True)
+    sizes_available = ItemSizeAvailableSerializer(many=True, source="model.items")
+    colors_available = ItemColorAvailableSerializer(many=True, source="model.items")
+
+    class Meta:
+        model = Item
+        fields = ("id", "model", "color", "size", "stock", "price", "stripe_product_id", "date_added", "images", "sizes_available", "colors_available")
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -33,3 +93,11 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "category", "fabric", "description", "date_added")
 
 
+class ProductListSerializer(ProductSerializer):
+    category = serializers.SlugRelatedField(many=False, read_only=True, slug_field="name")
+    max_price = serializers.DecimalField(max_digits=8, decimal_places=2)
+    items = ItemListSerializer(many=True, )
+
+    class Meta:
+        model = Product
+        fields = ("id", "name", "category", "fabric", "max_price", "description", "date_added", "items")
