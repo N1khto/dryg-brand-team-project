9eb1de4ee37f95827c734d0cdcbfbefabacf718f
Volodymyr Bohomol <8773927@gmail.com>
2023-12-28 16:27:31 +0200

    - made new migration - registered order models into admin panel

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -18,9 +18,15 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     status = models.CharField(max_length=63, choices=OrderStatusChoices.choices, default="awaiting payment")
 
+    def __str__(self):
+        return f"Order #{self.pk} from {self.order_date}. Total {self.total_price} usd. Status: {self.status}"
+
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="order_items")
     quantity = models.PositiveSmallIntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
+
+    def __str__(self):
+        return f"{self.item.model.name} - {self.quantity} pieces for {self.price}. Order #{self.order.id}"
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -8,6 +8,11 @@ class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def get_serializer_context(self) -> dict:
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
+
 
 class OrderItemViewSet(ModelViewSet):
     queryset = Order.objects.all()
