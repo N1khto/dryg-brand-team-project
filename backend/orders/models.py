6c893679b4fbd0958019959a4f532437bbd46873
Volodymyr Bohomol <8773927@gmail.com>
2024-01-01 14:49:42 +0200

    - added pricing logic to order models plus related migrations

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from products.models import Item
@@ -14,10 +15,17 @@ class Order(models.Model):
         CANCELED = "canceled"
         FAILED = "failed"
 
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="orders"
+    )
     order_date = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    status = models.CharField(max_length=63, choices=OrderStatusChoices.choices, default="awaiting payment")
+    status = models.CharField(
+        max_length=63, choices=OrderStatusChoices.choices, default="awaiting payment"
+    )
+
+    @property
+    def total_price(self):
+        return sum([_.item_price for _ in self.order_items.all()])
 
     def __str__(self):
         return f"Order #{self.pk} from {self.order_date}. Total {self.total_price} usd. Status: {self.status}"
@@ -25,9 +33,41 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="order_items")
-    quantity = models.PositiveSmallIntegerField()
-    price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
+    quantity = models.PositiveSmallIntegerField(default=1)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_items"
+    )
+
+    @staticmethod
+    def validate_item(quantity, item, error_to_raise):
+        if quantity > item.stock:
+            raise error_to_raise(
+                f"Requested amount of {item.model.name} is more than we have in stock"
+            )
+
+    def clean(self):
+        OrderItem.validate_item(
+            self.quantity,
+            self.item,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(OrderItem, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    @property
+    def item_price(self):
+        total_item_price = self.item.price * self.quantity
+        return total_item_price
 
     def __str__(self):
-        return f"{self.item.model.name} - {self.quantity} pieces for {self.price}. Order #{self.order.id}"
+        return f"{self.item.model.name} - {self.quantity} pieces for {self.item_price}. Order #{self.order.id}"
