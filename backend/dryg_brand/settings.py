08cc6234306da58e7762a546ff636b7afc907dc9
Volodymyr Bohomol <8773927@gmail.com>
2024-01-03 13:49:05 +0200

    - switch bd to postgresql - updated .env.sample

--- backend/dryg_brand/settings.py
+++ backend/dryg_brand/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = "dryg_brand.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
