1a5bc1a379a84317e7fe3c567275e2c8e89d095a
Volodymyr Bohomol <8773927@gmail.com>
2024-01-08 15:18:33 +0200

    - installed and configured debug toolbar and cors-headers - updated requirements.txt

--- backend/dryg_brand/settings.py
+++ backend/dryg_brand/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "django_filters",
+    "debug_toolbar",
+    "corsheaders",
     "user",
     "products",
     "orders",
@@ -57,7 +59,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -156,3 +160,7 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- backend/dryg_brand/urls.py
+++ backend/dryg_brand/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("api/shop/", include("products.urls", namespace="shop")),
     path("api/orders/", include("orders.urls", namespace="orders")),
     path("api/payments/", include("payments.urls", namespace="payments")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
