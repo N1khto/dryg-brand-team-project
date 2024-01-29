98b95894c355a16fdb64eeb11dc6cafc6457d4e9
Volodymyr Bohomol <8773927@gmail.com>
2024-01-29 16:27:05 +0200

    - added str method for image model

--- backend/products/models.py
+++ backend/products/models.py
@@ -92,3 +92,6 @@ class Image(models.Model):
     image = models.ImageField(upload_to="images/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"Description: {self.description}. Uploaded at: {self.uploaded_at}."
