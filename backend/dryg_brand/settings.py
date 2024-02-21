fe4d44729f3c4496be279d8a7c345194ec032198
Volodymyr Bohomol <8773927@gmail.com>
2024-02-21 13:23:50 +0200

    - edited static files settings #2

--- backend/dryg_brand/settings.py
+++ backend/dryg_brand/settings.py
@@ -151,7 +151,11 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STORAGES = {
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
 
 # MEDIA_ROOT = "/vol/web/media"  # docker media root
 MEDIA_ROOT = BASE_DIR / "media"  #offline maintenance media root
