ccda6bf44d1fa5e3173a03789951f473a69ef816
Volodymyr Bohomol <8773927@gmail.com>
2024-02-21 13:31:16 +0200

    - rollback static storage settings

--- backend/dryg_brand/settings.py
+++ backend/dryg_brand/settings.py
@@ -151,11 +151,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STORAGES = {
-        "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-        },
-    }
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA_ROOT = "/vol/web/media"  # docker media root
 MEDIA_ROOT = BASE_DIR / "media"  #offline maintenance media root
